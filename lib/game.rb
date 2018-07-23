9c43a5a32451b82cd86ffc0f827af3cd9d7fedb0
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-23 18:37:47 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -92,6 +92,7 @@ class Game
       end
       if input == "Human"
         @player_1 = Players::Human.new(token)
+        @player_2 = Players::Computer.new(token_2)
       end
     elsif players== "2"
       puts "Select token for Player 1"
